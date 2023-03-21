c0285946c0cd09f21209cf774f4e604e76c095fd
Justin Li <justinlit23@gmail.com>
2023-03-21 14:07:45 -0700

    Timer can be hidden and starts on its own

--- client/src/components/interfaceElements/Stopwatch.js
+++ client/src/components/interfaceElements/Stopwatch.js
@@ -1,8 +1,10 @@
+import "./stopwatch.css";
 import React, { useState, useEffect } from "react";
 
 function Stopwatch() {
-  const [isActive, setIsActive] = useState(false);
-  const [isPaused, setIsPaused] = useState(true);
+  const [isActive, setIsActive] = useState(true);
+  const [isPaused, setIsPaused] = useState(false);
+  const [isHidden, setIsHidden] = useState(false);
   const [time, setTime] = useState(0);
 
   React.useEffect(() => {
@@ -34,33 +36,43 @@ function Stopwatch() {
     setTime(0);
   }
 
+  function toggleHidden() {
+    setIsHidden(!isHidden);
+  }
+
   return (
     <div>
-      <div>
-        <div className="timer">
-          <span className="digits">
-            {("0" + Math.floor((time / 60000) % 60)).slice(-2)}:
-          </span>
-          <span className="digits">
-            {("0" + Math.floor((time / 1000) % 60)).slice(-2)}.
-          </span>
-          <span className="digits mili-sec">
+      {isHidden ? null : (
+        <div>
+          <div className="timer">
+            <span className="digits">
+              {("0" + Math.floor((time / 60000) % 60)).slice(-2)}:
+            </span>
+            <span className="digits">
+              {("0" + Math.floor((time / 1000) % 60)).slice(-2)}
+            </span>
+            {/* <span className="digits mili-sec">
             {("0" + ((time / 10) % 100)).slice(-2)}
-          </span>
+          </span> */}
+          </div>
         </div>
-      </div>
-
+      )}
       <div>
+        <button onClick={toggleHidden} id="hide">
+          {isHidden ? "SHOW" : "HIDE"}
+        </button>
+        <br></br>
         {!isPaused ? (
           <button onClick={handlePause} id="pause">
             PAUSE
           </button>
         ) : (
           <button onClick={handleStart} id="start">
-            START
+            RESUME
           </button>
         )}
 
+        <br></br>
         <button onClick={handleReset} id="reset">
           RESET
         </button>
